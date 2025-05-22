379625b86e5311a96e762a0b7891284cd6e76721
Ivs270191 <152504971+Ivs270191@users.noreply.github.com>
2025-05-22 16:15:56 +0300

    last correction for deploy

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -27,10 +27,13 @@ export const ProductsGroupList: React.FC<Props> = ({
   className,
 }) => {
   const setActiveCategoryId = UseCategoryStore((state) => state.setActiveId);
-  const intersectionRef = React.useRef(null);
-  const intersection = useIntersection(intersectionRef, {
-    threshold: 0.4,
-  });
+  const intersectionRef = React.useRef<HTMLDivElement>(null);
+  const intersection = useIntersection(
+    intersectionRef as React.RefObject<HTMLDivElement>,
+    {
+      threshold: 0.4,
+    }
+  );
 
   React.useEffect(() => {
     if (intersection?.isIntersecting) {
@@ -39,7 +42,11 @@ export const ProductsGroupList: React.FC<Props> = ({
   }, [categoryId, intersection?.isIntersecting, title]);
 
   return (
-    <div className={className} id={title} ref={intersectionRef}>
+    <div
+      className={className}
+      id={title}
+      ref={intersectionRef as React.RefObject<HTMLDivElement>}
+    >
       <Title text={title} size="lg" className="font-extrabold mb-5" />
 
       <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
